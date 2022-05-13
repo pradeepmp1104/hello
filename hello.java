1cb2a43c6bd45fe7bae726b73d888fecbe6d9dae
pradeepmp1104 <105268610+pradeepmp1104@users.noreply.github.com>
2022-05-13 17:20:24 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,15 @@
-class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }  
+class Main {
+
+  public static void main(String[] args) {
+    
+    System.out.println("Enter two numbers");
+    int first = 10;
+    int second = 20;
+    
+    System.out.println(first + " " + second);
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println("The sum is: " + sum);
+  }
 }
