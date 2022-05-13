c01d5b75c4f35575a9fed8f451f0b90c72801b20
pradeepmp1104 <105268610+pradeepmp1104@users.noreply.github.com>
2022-05-13 17:04:38 +0530

    code change

--- hello.java
+++ hello.java
@@ -1,21 +1,4 @@
-import java.util.Scanner;
- 
-public class SwapTwoNumbers {
- 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        int x, y, temp;
-        System.out.println("Enter x and y");
-        Scanner in = new Scanner(System.in);
-        x = in.nextInt();
-        y = in.nextInt();
-        System.out.println("Before Swapping" + x + y);
-        temp = x;
-        x = y;
-        y = temp;
-        System.out.println("After Swapping" + x + y);
-     system.out.println("hi hello the world");
-         
-    }
- 
-}
+public class Simple{
+public static void main(String args[]){
+System.out.println("hello javatpoint");
+}}
